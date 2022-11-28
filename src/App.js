c3998c03e35cb0806b5f9dc326ac0ea78b2931be
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-11-28 07:58:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+import Password from "./input";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Password />
+    </div>
+  );
+}
